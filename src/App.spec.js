1bc1e95fbcb8f8d9476afe24b3312ecb34a8ec9d
Ravi Teja D <ravi1917d@gmail.com>
2019-01-20 19:37:05 +0530

    RED STATE - Added New Test for Calculator Component which fails

--- src/App.spec.js
+++ src/App.spec.js
@@ -3,8 +3,16 @@ import { shallow } from "enzyme";
 import App from "./App";
 
 describe("AppComponent", () => {
+  let wrapper;
+  beforeEach(() => (wrapper = shallow(<App />)));
+
   it("should render a <div />", () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find("div").length).toEqual(1);
+  });
+
+  it("should render the Calculator Component", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.containsMatchingElement(<Calculator />)).toEqual(true);
   });
 });
