2ece5744fcd6c15f71f8ea3c5f1701048980c9a3
Ravi Teja D <ravi1917d@gmail.com>
2019-01-20 19:30:56 +0530

    REFACTOR STATE - Moved App.css to /css/ and added className app-container to div

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 
-import "./App.css";
+import "./css/App.css";
 
 class App extends Component {
   render() {
-    return <div />;
+    return <div className="app-container" />;
   }
 }
 
