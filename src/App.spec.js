e828125d269eba971bd1704993b75f2d287b07fe
Ravi Teja D <ravi1917d@gmail.com>
2019-01-20 19:13:37 +0530

    RED-STATE - Added a failing test case for App

---
+++ src/App.spec.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { shallow } from "enzyme";
+import App from "./App";
+
+describe("AppComponent", () => {
+  it("should render a <div />", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("div").length).toEqual(1);
+  });
+});
