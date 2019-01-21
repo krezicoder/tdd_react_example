229a57bb0983074ed2ad87269d7d84afbc04bbfd
Ravi Teja D <ravi1917d@gmail.com>
2019-01-21 11:03:42 +0530

    RED STATE - Keypad expecting div test case added

---
+++ src/components/Keypad/Keypad.spec.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Keypad from "./Keypad";
+
+describe("Keypad", () => {
+  let wrapper;
+  beforeEach(() => (wrapper = shallow(<Keypad />)));
+
+  it("should render a <div />", () => {
+    expect(wrapper.find("div").length).toEqual(1);
+  });
+});
