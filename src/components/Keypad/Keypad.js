80a3f5da3cc1a2e76b8e8c37b7c9bdecaade49ed
Ravi Teja D <ravi1917d@gmail.com>
2019-01-21 15:40:25 +0530

    GREEN STATE For keys rendering on the calculator

--- src/components/Keypad/Keypad.js
+++ src/components/Keypad/Keypad.js
@@ -2,17 +2,19 @@ import React, { Component } from "react";
 import Key from "../Key/Key";
 class Keypad extends Component {
   render() {
-    const { numbers } = this.props;
-    const numberKeys = numbers.map(number => <p key={number}>{number}</p>);
-    const { operators } = this.props;
+    const { numbers, operators } = this.props;
+    const numberKeys = numbers.map(number => (
+      <Key key={number} keyValue={number} keyType={"number-key"} />
+    ));
+
     const operatorKeys = operators.map(operator => (
-      <p key={operator}>{operator}</p>
+      <Key key={operator} keyValue={operator} keyType={"operator-key"} />
     ));
     return (
       <div className="keypad-container">
         <div className="numbers-container">{numberKeys}</div>
         <div className="operators-container">{operatorKeys}</div>
-        <Key />
+        <Key keyType={"submit-key"} keyValue={"="} />
       </div>
     );
   }
--- src/components/Keypad/Keypad.spec.js
+++ src/components/Keypad/Keypad.spec.js
@@ -10,17 +10,16 @@ describe("Keypad", () => {
     expect(wrapper.find("div").length).toEqual(3);
   });
 
-  it("renders the values of numbers", () => {
-    wrapper.setProps({ numbers: ["0", "1", "2"] });
-    expect(wrapper.find(".numbers-container").text()).toEqual("012");
-  });
-
-  it("renders the values of operators", () => {
-    wrapper.setProps({ operators: ["+", "-", "*", "/"] });
-    expect(wrapper.find(".operators-container").text()).toEqual("+-*/");
-  });
-
   it("should render an instance of the Key component", () => {
     expect(wrapper.find("Key").length).toEqual(1);
+  });
+
+  it("should render an instance of the Key component for each index of numbers, operators, and the submit Key", () => {
+    const numbers = ["0", "1", "2", "3"];
+    const operators = ["+", "-"];
+    const submit = 1;
+    const keyTotal = numbers.length + operators.length + submit;
+    wrapper.setProps({ numbers, operators });
+    expect(wrapper.find("Key").length).toEqual(keyTotal);
   });
 });
